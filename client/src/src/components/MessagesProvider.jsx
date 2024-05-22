b95cf75bf29fbc66a887e1c8ab7f56ed2f3e1a68
BrandonMA <brandonma98@protonmail.com>
2024-05-22 19:44:06 +0200

    feat: add initial message specifiying that it is a bot

--- client/src/src/components/MessagesProvider.jsx
+++ client/src/src/components/MessagesProvider.jsx
@@ -1,8 +1,14 @@
 import { useState } from "react";
 import { MessagesContext } from "../context";
+import { AUTHOR_TYPES } from "../types";
 
 export const MessagesProvider = ({ children }) => {
-  const state = useState([]);
+  const state = useState([
+    {
+      author: AUTHOR_TYPES.BOT,
+      body: "¡Hola! Soy un chatbot de Sabentis y estoy aquí para ayudarte",
+    },
+  ]);
   return (
     <MessagesContext.Provider value={state}>
       {children}
--- client/src/src/context.js
+++ client/src/src/context.js
@@ -1,3 +1,3 @@
 import { createContext } from "react";
 
-export const MessagesContext = createContext();
+export const MessagesContext = createContext([]);
